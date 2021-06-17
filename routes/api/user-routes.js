e803f00f131e928697c43ca1b6bca10b1a4af51c
Lucile <developer.lvt@gmail.com>
2021-06-16 22:09:55 -0400

    created dedicated api routes folder

---
+++ routes/api/user-routes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const  User  = require('../../models/User');
+
+//READ (FIND) REQUESTS
+router.get("/", (req, res) => {
+    User.find({})
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+  router.get("/:userid", (req, res) => {
+    User.findById(req.params.userid)
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+  //POST CREATE REQUESTS
+  router.post("/", (req, res) => {
+    User.create(req.body)
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+  //UPDATE PUT REQUESTS
+  router.put("/:userid", (req, res) => {
+    User.findByIdAndUpdate(req.params.userid, req.body, { new: true })
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+  //DELETE DELETE REQUESTS
+  router.delete("/:userid", (req, res) => {
+    User.findOneAndDelete({ _id: req.params.userid })
+      .then((data) => {
+        res.status(200).json(data);
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  });
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,61 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const User = require('./models/User');
+const User = require("./models/User");
 
 const app = express();
 const PORT = 3001;
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-mongoose.connect("mongodb://localhost/social_network_db", {
-  useFindAndModify: false, 
-  useNewUrlParser:true,
-  useUnifiedTopology:true});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/social_network_db",
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 mongoose.set("useCreateIndex", true);
 mongoose.set("debug", true);
 
-//READ (FIND) REQUESTS
-app.get('/api/users', (req, res) => {
-  User.find({}).then(data => {
-    res.status(200).json(data)
-  }).catch(err => {
-    res.status(500).json(err);
-  })
-})
-app.get('/api/users/:userid', (req, res) => {
-  User.findById(req.params.userid).then(data => {
-    res.status(200).json(data)
-  }).catch(err => {
-    res.status(500).json(err);
-  })
-})
-//POST CREATE REQUESTS
-app.post('/api/users', (req, res) => {
-  User.create(req.body).then(data => {
-    res.status(200).json(data)
-  }).catch(err => {
-    res.status(500).json(err);
-  })
-})
-//UPDATE PUT REQUESTS
-app.put('/api/users/:userid', (req, res) => {
-  User.findByIdAndUpdate(req.params.userid, req.body, { new: true }).then(data => {
-    res.status(200).json(data)
-  }).catch(err => {
-    res.status(500).json(err);
-  })
-})
-//DELETE DELETE REQUESTS
-app.delete('/api/users/:userid', (req, res) => {
-  User.findOneAndDelete({ _id: req.params.userid }).then(data => {
-    res.status(200).json(data)
-  }).catch(err => {
-    res.status(500).json(err);
-  })
-})
+
+app.use(require('./routes'));
 
 app.listen(PORT, () => {
-  console.log(`Listening on PORT: ${PORT}`)
-})
+  console.log(`Listening on PORT: ${PORT}`);
+});
