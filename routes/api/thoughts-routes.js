b3b541a6d6f826e5ea799b43f5b073b5eca7d4bd
Lucile <developer.lvt@gmail.com>
2021-06-24 14:16:01 -0400

    created association to post a thought along th user who created it

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -1,34 +1,43 @@
-const router = require('express').Router();
-const  Thought  = require('../../models/Thought');
+const router = require("express").Router();
+const Thought = require("../../models/Thought");
+const User = require("../../models/User");
 
 router.get("/", (req, res) => {
-    Thought.find({})
-      .then((data) => {
-        res.status(200).json(data);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
-      });
-  });
+  Thought.find({})
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
-  router.get("/:thoughtid", (req, res) => {
-    Thought.findById(req.params.thoughtid)
-      .then((data) => {
-        res.status(200).json(data);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
-      });
-  });
+router.get("/:thoughtid", (req, res) => {
+  Thought.findById(req.params.thoughtid)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
-  router.post("/", (req, res) => {
-    Thought.create(req.body)
-      .then((data) => {
-        res.status(200).json(data);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
-      });
-  });
+router.post("/", (req, res) => {
+  Thought.create(req.body)
+  .then(data => {
+    console.log(data);
+      return User.findOneAndUpdate(
+          {username: req.body.username}, 
+          { $push: { thoughts: data._id } }, 
+          { new: true }
+        );
+    })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
 
 module.exports = router;
