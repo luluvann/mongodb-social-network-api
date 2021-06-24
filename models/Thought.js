d341a67ff35ca3c73e0e332383b0ec20f6dc530f
Lucile <developer.lvt@gmail.com>
2021-06-24 17:49:01 -0400

    created route to create reaction to existing thought

--- models/Thought.js
+++ models/Thought.js
@@ -1,45 +1,42 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const ReactionSchema = new Schema({
   reactionBody: {
     type: String,
     required: true,
-    maxLength: 280
+    maxLength: 280,
   },
   username: {
     type: String,
-    required: 'Username who created the reaction is required'
+    required: "Username who created the reaction is required",
   },
   createdAt: {
     type: Date,
-    default:Date.now,
-  }
-})
+    default: Date.now,
+  },
+});
 
 const ThoughtSchema = new Schema({
   thoughtText: {
     type: String,
-    required: 'Thought Text is required',
+    required: "Thought Text is required",
     minLength: 1,
-    maxLength: 280
+    maxLength: 280,
   },
 
   createdAt: {
     type: Date,
-    default:Date.now,
+    default: Date.now,
   },
 
   username: {
     type: String,
-    required: 'Username who created the thought is required'
+    required: "Username who created the thought is required",
   },
 
-  reactions:{
-    ReactionSchema
-  }
-
+  reactions: [{ ReactionSchema }],
 });
 
-const Thought = model('Thought', ThoughtSchema);
+const Thought = model("Thought", ThoughtSchema);
 
 module.exports = Thought;
--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -27,13 +27,13 @@ router.get("/:thoughtid", (req, res) => {
 // POST NEW THOUGHT
 router.post("/", (req, res) => {
   Thought.create(req.body)
-  .then(data => {
-    console.log(data);
+    .then((data) => {
+      console.log(data);
       return User.findOneAndUpdate(
-          {username: req.body.username}, 
-          { $push: { thoughts: data._id } }, 
-          { new: true }
-        );
+        { username: req.body.username },
+        { $push: { thoughts: data._id } },
+        { new: true }
+      );
     })
     .then((data) => {
       res.status(200).json(data);
@@ -46,6 +46,22 @@ router.post("/", (req, res) => {
 //DELETE THOUGHT by ID
 router.delete("/:thoughtid", (req, res) => {
   Thought.findOneAndDelete({ _id: req.params.thoughtid })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+//CREATE REACTION to exisiting thought
+router.post("/:thoughtid/reactions/add", (req, res) => {
+  console.log(req.body)
+  Thought.findByIdAndUpdate(
+    req.params.thoughtid,
+    { $push: { reactions: req.body } },
+    { new: true }
+  )
     .then((data) => {
       res.status(200).json(data);
     })
