101f377c671f901d3b63f257d27b588f158628d5
Lucile <developer.lvt@gmail.com>
2021-06-26 16:16:57 -0400

    added query to delete associated thoughts when a user is deleted

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,13 +1,10 @@
 const router = require("express").Router();
 const User = require("../../models/User");
+const Thought = require("../../models/Thought");
 
 //GET USERS
 router.get("/", (req, res) => {
   User.find()
-    .populate({
-      path: "thoughts",
-      select: "-__v",
-    })
     .then((data) => {
       res.status(200).json(data);
     })
@@ -56,6 +53,15 @@ router.put("/:userid", (req, res) => {
 router.delete("/:userid", (req, res) => {
   User.findOneAndDelete({ _id: req.params.userid })
     .then((data) => {
+      //delete associated thoughts to the deleted user
+      Thought.deleteMany({ username: { $eq: data.username } })
+        .then((data) => {
+          res.status(200).json(data);
+        })
+        .catch((err) => {
+          res.status(500).json(err);
+        });
+
       res.status(200).json(data);
     })
     .catch((err) => {
@@ -77,7 +83,7 @@ router.post("/:userid/friends/:friendid", (req, res) => {
       res.status(500).json(err);
     });
 
-  //when a user is adding another user as a friend, the first user gets also added as a friend to the user he/she is adding (mutuall adding)  
+  //when a user is adding another user as a friend, the first user gets also added as a friend to the user he/she is adding (mutuall adding)
 
   User.findByIdAndUpdate(
     req.params.friendid,
@@ -106,17 +112,19 @@ router.delete("/:userid/friends/:friendid", (req, res) => {
       res.status(500).json(err);
     });
 
-    User.findByIdAndUpdate(
-      req.params.friendid,
-      { $pull: { friends: req.params.userid } },
-      { new: true }
-    )
-      .then((data) => {
-        res.status(200).json(data);
-      })
-      .catch((err) => {
-        res.status(500).json(err);
-      });
+  //when a user is deleted, that user is removed from his/her friends's list
+
+  User.findByIdAndUpdate(
+    req.params.friendid,
+    { $pull: { friends: req.params.userid } },
+    { new: true }
+  )
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
